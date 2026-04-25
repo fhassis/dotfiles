94c91b179e354298aefde974f850d51d23e95dcb
fhassis <fhassis2@gmail.com>
2026-04-25 14:43:29 -0300

    refactor: update SSH agent configuration and clean up unused files

--- fish/.config/fish/conf.d/containerenv.fish
+++
@@ -1,8 +0,0 @@
-# Detect if the shell is running inside a container (Toolbx/Podman)
-if test -f /run/.containerenv
-    # Set icon for the Dev Container environment
-    set -gx STARSHIP_DISTRO "📦"
-else
-    # Set icon for the Atomic Fedora Host
-    set -gx STARSHIP_DISTRO "❄️"
-end
--- fish/.config/fish/conf.d/ssh-agent.fish
+++ fish/.config/fish/conf.d/ssh-agent.fish
@@ -1,4 +1,10 @@
-# configure ssh agent
+# Configure SSH agent depending on environment
 if status is-interactive
-    set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
+    if test -n "$WSL_DISTRO_NAME"
+        # Running inside WSL → use Windows OpenSSH agent
+        set -gx SSH_AUTH_SOCK /mnt/c/Users/fhassis/AppData/Local/ssh-agent/ssh-agent.sock
+    else
+        # Native Linux → use local ssh-agent socket
+        set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
+    end
 end
